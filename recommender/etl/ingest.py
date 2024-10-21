624817b24bae98745e9e709e14cf327eb4bb4687
pjvalent <petervalentine23@gmail.com>
2024-10-20 21:02:53 -0700

    just exploring the data

--- recommender/etl/ingest.py
+++ recommender/etl/ingest.py
@@ -1,7 +1,11 @@
-
-
+import pandas as pd
+import numpy as np
 
 def main():
+    data = {'Name': ['Peter', 'Kayla', 'Cheese']}
+    df = pd.DataFrame(data)
+
+    print(df)
     print("Hello world!")
 
 
